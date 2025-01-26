2060b109703832502220d5aaff932210d20de72e
dnest2009 <dnest2009@gmail.com>
2025-01-26 16:21:50 +0200

    add try / except

--- get_days_from_today.py
+++ get_days_from_today.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 
 def get_days_from_today(date:str):  #вхідні дані - дата у форматі:"string"
-    today = datetime.today()        #поточна дата
-    now_date = datetime.strptime(date,"%Y-%m-%d")   #перетворення вхідної дати у об'єкт datatime
-    delta_days = (now_date - today).days            #різниця між вхідною і поточною датами у днях
-    return delta_days                               #повернення результату ф-ії
+    try:
+        today = datetime.today()        #поточна дата
+        now_date = datetime.strptime(date,"%Y-%m-%d")   #перетворення вхідної дати у об'єкт datatime
+        delta_days = (now_date - today).days            #різниця між вхідною і поточною датами у днях
+        return delta_days                               #повернення результату ф-ії
+    except:
+        return "неправильний формат даних"
+
+day = "24-01-2009"
+x = get_days_from_today(day)
+print (x)
