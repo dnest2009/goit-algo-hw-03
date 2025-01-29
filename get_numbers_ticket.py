40c6836ad91d0e87c47a59ac115d3f9ce4bdddb9
dnest2009 <dnest2009@gmail.com>
2025-01-29 12:23:02 +0200

    add

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -4,15 +4,17 @@ def get_numbers_ticket(min, max, quantity):
     get_numbers_ticket=[]  #створюємо пустий список
     i=int(0)  #присвоюємо лічильнику початкове значення
     try:      
-        if min <= quantity <= max and min>=1 and max<=1000:   #перевірка вхідних параметрів
+        if max>min and min>=1 and max<=1000 and quantity<=max-min+1:   #перевірка вхідних параметрів 
             while i < quantity:          
                 x = random.randint(min, max)      #рандомне число між min та max
                 if x not in get_numbers_ticket:        #перевірка чи є вже дане число у списку
                     get_numbers_ticket.append(x)      #додати число у список
                     i=i+1                            #лічильник +1
-        get_numbers_ticket.sort()                #сортування чисел у списку за зростанням
-        return get_numbers_ticket              #результат список чисел
+            get_numbers_ticket.sort()                #сортування чисел у списку за зростанням
+            return get_numbers_ticket             #результат список чисел
+        else:
+            return get_numbers_ticket              #результат пустий список, невірний формат вхідних параметрів
     except:
-        return get_numbers_ticket              #результат пустий список
+        return get_numbers_ticket              #результат пустий список, невірний формат вхідних параметрів
 
-# print(get_numbers_ticket(1,1000,5))
+print(get_numbers_ticket(10,6,10))
