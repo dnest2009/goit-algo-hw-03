29201bb04230e9693e21f63384c6f96fe1164437
dnest2009 <dnest2009@gmail.com>
2025-01-31 20:32:51 +0200

    конвертовано змінну today - today_date - t

--- get_days_from_today.py
+++ get_days_from_today.py
@@ -1,12 +1,14 @@
-from datetime import datetime
+from datetime import datetime,timedelta
 
-def get_days_from_today(date:str):  #вхідні дані - дата у форматі:"string"
+def get_days_from_today(when_this_day:str):  #вхідні дані - дата у форматі:"string"
     try:                                #на випадок отримання даних у не правильному форматі
-        today = datetime.today().day        #поточна дата у форматі ДЕНЬ
-        now_date = datetime.strptime(date,"%Y-%m-%d").day   #перетворення вхідної дати у об'єкт datatime ДЕНЬ
-        delta_days = now_date - today     #різниця між вхідною і поточною датами у ДНЯХ
-        return delta_days   #повернення результату ф-ії
+        today = datetime.today()     #поточна дата у форматі datetime
+        today_date = today.strftime("%Y-%m-%d") # перетворення поточної дати у рядок
+        t=datetime.strptime(today_date,"%Y-%m-%d")  # перетворення поточної дати у об'єкт datetime без вказання годин, секунд і т.д
+        now_date = datetime.strptime(when_this_day,"%Y-%m-%d")  #перетворення вхідної дати у об'єкт datatime
+        delta_days = (now_date - t).days   #різниця між вхідною і поточною датами у ДНЯХ
+        return delta_days #повернення результату ф-ії
     except:
-        return "неправильний формат даних"  #повідомлення про неправильний формат даних
+       return  "неправильний формат даних"  #повідомлення про неправильний формат 
 
 print(get_days_from_today("2025-01-31"))
